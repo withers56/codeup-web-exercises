d1f126d4fca412cdff5420b288510e97734b9e88
withers56 <william.k.withers@gmail.com>
2021-12-08 11:59:38 -0600

    adds hello-asteroid.js

---
+++ js/hello-asteroid.js
@@ -0,0 +1,18 @@
+console.log("Hello, sweet asteroid!");
+
+//string, number, (bigint), boolean, undefined, null, object
+
+console.log("Hello, this " + "isn't a world");
+
+var string1 = "fresh ";
+var favoriteFood = "pizza";
+
+console.log("One of my favorite foods is " + favoriteFood + " because " + string1 +
+"melted " + "cheese " + "is the best!");
+
+favoriteFood = "enchiladas";
+string1 = "just kidding, i NEED ";
+
+console.log( string1 + favoriteFood );
+
+console.log("");
