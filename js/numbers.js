d80feb839f81259f0959a9817c0ea4ed386e7604
withers56 <william.k.withers@gmail.com>
2021-12-09 11:17:55 -0600

    updates

---
+++ js/numbers.js
@@ -0,0 +1,38 @@
+//console.log(1 + 2);// added a number to another number
+// integer, floating-point number, infinity (1/0), real/imaginary numbers
+
+//console.log((1 + 3.14).toFixed(2));
+
+// Parenthesis, Exponents, Multiply,Divide, Add/Subtract. PEMDAS.
+
+// let myExpression = (10 * 2 / 5) - 2**2;
+//10 * 2 = 20
+//20 / 5 = 4
+//2 ^ 2 = 4
+// 4 - 4 = 0
+
+// console.log((1 / myExpression)); // OOF, infinity considered a number
+
+// binary operator
+//requires TWO (binary) operands in order to evaluate
+// + add
+// - subtract
+// * multiply
+// / divide
+// % modulus
+// ** ^ exponential
+
+// unary operators
+// + plus
+// - negate
+// ++ increment
+// -- decrement
+// typeof
+let num1 = 22;
+console.log(num1++); // post-increment
+// num1 original is output to the console, then incremented
+console.log(num1);
+console.log(++num1); // pre-increment
+
+// num1 = num1 +1;
+// num1 += 1;
