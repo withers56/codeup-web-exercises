ca95723381c8f3bf65138f94ba3f939c92b6be84
withers56 <william.k.withers@gmail.com>
2021-12-10 09:36:42 -0600

    warmup

---
+++ js/js-warmups.js
@@ -0,0 +1,20 @@
+// Using the two provided variables, create a piece of logic which will
+// determine if a person is qualified to be extended an auto loan.
+
+//     Requirements:
+// Their credit score must be over 680 AND have at least $4000 on-hand
+// OR, they must have at least $10000 on-hand
+// Scenario:
+//     I have $12000 on-hand but have a 590 credit score. I get a loan.
+//     I have $5000 on-hand with a 720 credit score. I get a loan
+//     I have $3000 on-hand with a 720 credit score. I do not get a loan.
+//     I have $9000 on-hand with a 590 credit score. I do not get a loan.
+
+var creditScore = 590;
+var cashOnHand = 12000;
+
+if (((creditScore > 680) && (cashOnHand >= 4000)) || (cashOnHand >= 10000)) {
+    console.log("You have been qualified for the auto loan");
+} else console.log("You did not qualify for the auto loan");
+
+///////////////////////////////////////////////////////////////////////////
