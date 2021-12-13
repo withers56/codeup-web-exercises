2d5068c090f985ade4f5e21bbb2dd344975c711c
withers56 <william.k.withers@gmail.com>
2021-12-13 09:32:34 -0600

    warmups

--- js/js-warmups.js
+++ js/js-warmups.js
@@ -10,11 +10,21 @@
 //     I have $3000 on-hand with a 720 credit score. I do not get a loan.
 //     I have $9000 on-hand with a 590 credit score. I do not get a loan.
 
-var creditScore = 590;
-var cashOnHand = 12000;
-
-if (((creditScore > 680) && (cashOnHand >= 4000)) || (cashOnHand >= 10000)) {
-    console.log("You have been qualified for the auto loan");
-} else console.log("You did not qualify for the auto loan");
+// var creditScore = 590;
+// var cashOnHand = 12000;
+//
+// if (((creditScore > 680) && (cashOnHand >= 4000)) || (cashOnHand >= 10000)) {
+//     console.log("You have been qualified for the auto loan");
+// } else console.log("You did not qualify for the auto loan");
 
 ///////////////////////////////////////////////////////////////////////////
+
+// Declare a function called 'isFive' that takes in an input and returns
+// true if that input is the number five. Otherwise it should return false.
+
+function isFive(number) {
+    if (number == 5) return true;
+    else return false;
+}
+
+console.log(isFive(5));
