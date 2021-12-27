7ed73227e104604bc3ff780e632ea55c70fa2eba
withers56 <william.k.withers@gmail.com>
2021-12-27 16:36:56 -0600

    finished iterating exercises

--- js/iterating.js
+++ js/iterating.js
@@ -7,29 +7,39 @@
      * 'names'.
      */
 
+    let names = ["william", "hailey", "raymond", "megan"];
+    //console.log(names[0]);
+
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
-
+    console.log(names.length);
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
-
+    // console.log(names[0]);
+    // console.log(names[1]);
+    // console.log(names[2]);
+    // console.log(names[3]);
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
-
+    // for(var i = 0; i < names.length; i++) {
+    //     console.log(names[i]);
+    // }
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
-
+    // names.forEach(function (input){
+    //     console.log(input);
+    // });
     /**
      * TODO:
      * Create the following three functions, each will accept an array and
@@ -43,5 +53,17 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+    function firstElement(array){
+        return array[0];
+    }
+    function secondElement(array) {
+        return array[1];
+    }
+    function lastElement(array) {
+        return array[array.length - 1];
+    }
 
+    console.log(firstElement(names));
+    console.log(secondElement(names));
+    console.log(lastElement(names));
 })();
