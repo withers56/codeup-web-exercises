1eba39ec902d2b363b92254f96a8c79b38475fd0
withers56 <william.k.withers@gmail.com>
2022-01-06 10:12:05 -0600

    Objects lecture

---
+++ js/objects-lec.js
@@ -0,0 +1,32 @@
+"use strict";
+
+let carMakes = ["Ford", "Toyota"];
+let carModels = ["Taurus", "Tundra"]; // not this
+
+
+
+let car1 = {};
+car1.make = 'Ford';// make = property, attribute, member variable
+car1.model = 'Taurus';
+car1.year = 2000;
+
+console.log(car1);
+
+let car2 = {
+    make : 'Toyota',
+    model : 'Tundra',
+    year : 2007
+};
+
+console.log(car2);
+
+console.log("Make: " + car1.make);
+console.log("Model: " + car1.model);
+console.log("Year: " + car1.year);
+
+
+
+
+
+
+
