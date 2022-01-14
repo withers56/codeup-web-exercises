20b44405054f9fd959d3c3fe2b89c2bf15b1649b
withers56 <william.k.withers@gmail.com>
2022-01-14 09:49:44 -0600

    Js warmup exercise

--- js/js-warmups.js
+++ js/js-warmups.js
@@ -358,23 +358,44 @@ function forLoopIterate2(input) {
 //     It returns a string that is the concatenation of all the elements in myArray.
 //     This function is essentially your own version of join.
     // Therefore, you cannot use join in your function body :slightly_smiling_face:
-let someArray = ["this", "is", "a", "test", "array"]
+// let someArray = ["this", "is", "a", "test", "array"]
+//
+// function arrayToString (myArray){
+//     let myString = '';
+//     for (let i = 0; i < myArray.length; i++) {
+//         myString = myString.concat(myArray[i] + " ");
+//     }
+//     return myString;
+// }
+//
+// console.log(arrayToString(someArray));
 
-function arrayToString (myArray){
-    let myString = '';
-    for (let i = 0; i < myArray.length; i++) {
-        myString = myString.concat(myArray[i] + " ");
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Define a function named removeAll(array, value) that takes in two arguments. The first argument should be an array
+// and the second argument should be a value you wish to remove
+// Given the following array as an argument (do not define this array inside your function - pass it in as an argument)
+// var bugs = ["mosquito", "ant", "scorpion", "ant", "ant", "mosquito", "typo", "reference error", "type error"];
+// removeAll(bugs, "ant") should return ["mosquito", "scorpion", "mosquito", "typo", "reference error", "type error"]
+// removeAll(bugs, "mosquito") should return ["ant", "scorpion", "ant", "ant", "typo", "reference error", "type error"]
+// removeAll(bugs, "roach") should return the original array b/c “roach” has no occurrances.
+
+var bugs = ["mosquito", "ant", "scorpion", "ant", "ant", "mosquito", "typo", "reference error", "type error"];
+
+function removeAll(array, value) {
+    let tempArray = [];
+    for (let i = 0; i < array.length; i++) {
+
+        if (value !== array[i]) {
+            tempArray.push(array[i]);
+        }
     }
-    return myString;
+    return tempArray;
 }
 
-console.log(arrayToString(someArray));
-
-
-
-
-
-
+console.log(removeAll(bugs, "ant"));
+console.log(removeAll(bugs, "mosquito"));
+console.log(removeAll(bugs, "roach"));
 
 
 
