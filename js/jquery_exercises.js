cd68f70e50214896c426841dae19e270d5db3c4e
withers56 <william.k.withers@gmail.com>
2022-02-03 10:58:00 -0600

    COmpeted jquery selectors esxercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,20 +1,18 @@
-'use strict';
+(function (){
+    'use strict';
 
-$(document).ready(function () {
+    $(document).ready(function () {
+        console.log('DOM READY');
+        console.log(`jQuery version : ${$.fn.jquery}`);
 
-
-
-
-
-
-
-
+        let mainDiv = $('#main-box').html();
+        // alert(mainDiv)
 
 
-
-
-
-
+        $('.codeup').css('border', '1px solid red');
+        $('li').css('font-size', '20px');
+        $('h1, p, li').css('background-color', 'yellow');
+        alert($('h1').html());
 
 
 
@@ -27,4 +25,13 @@ $(document).ready(function () {
 
 
 
-});
+
+
+
+
+
+
+
+
+    });
+}());
