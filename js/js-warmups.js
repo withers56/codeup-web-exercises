6a73f68879cb7a031a56d9f4f60d945ebe20bd47
withers56 <william.k.withers@gmail.com>
2022-02-04 09:25:19 -0600

    finished konami exercise and warmuo

--- js/js-warmups.js
+++ js/js-warmups.js
@@ -608,3 +608,18 @@ function forLoopIterate2(input) {
 
 
 
+// Write a function called convertLowHighToObject that takes in a string  that represents the low and high temperature
+// in this format '35, 42' (assuming low will always be the left value and high the right) and returns an object with the
+//     properties 'low' and 'high' with the values of those respective properties set to the corresponding values in the input string.
+//     NOTE: the values in the object should be of the number type
+// Example input: '35, 42'
+// Example return: {low: 35, high: 42}
+
+
+function convertLowHighToObject (lowHighString) {
+    let tempArray = lowHighString.split(', ');
+    return {low: parseInt(tempArray[0]), high: parseInt(tempArray[1])};
+
+}
+
+console.log(convertLowHighToObject('35, 42'));
