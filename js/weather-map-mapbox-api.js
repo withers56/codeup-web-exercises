6eacc890606dd2dab124e786ef22b2bed04613e7
withers56 <william.k.withers@gmail.com>
2022-02-10 15:21:01 -0600

    Major functions of weather map project completed

--- js/weather-map-mapbox-api.js
+++ js/weather-map-mapbox-api.js
@@ -3,17 +3,20 @@
 mapboxgl.accessToken = MAPBOXGL_ACCESS_TOKEN;
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
+    style: 'mapbox://styles/mapbox/dark-v10',
     center: [-98.4916, 29.4252],
     zoom: 10
 });
 
 // Add the control to the map.
+const mapboxgeocoder = new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl,
+    marker: false
+})
+
 map.addControl(
-    new MapboxGeocoder({
-        accessToken: mapboxgl.accessToken,
-        mapboxgl: mapboxgl
-    })
+    mapboxgeocoder
 );
 
 function createMarker (lng, lat) {
@@ -21,4 +24,26 @@ function createMarker (lng, lat) {
         .setLngLat([lng, lat])
         .addTo(map);
 }
- createMarker(-98, 29)
+
+function getLngLat (results) {
+    getLocationData(results[0], results[1]);
+}
+
+
+mapboxgeocoder.on('result', function (results){
+    console.log(results.result.place_name);
+    geocode(`${results.result.place_name}`, MAPBOXGL_ACCESS_TOKEN).then(function (result){
+        getLocationData(result[1], result[0]);
+        createMarker(result[0], result[1]);
+    });
+
+});
+
+map.on('click', function (result){
+    console.log(result.lngLat);
+    getLocationData(result.lngLat.lat, result.lngLat.lng);
+})
+
+
+
+
--- js/weather_map_jQuery.js
+++ js/weather_map_jQuery.js
@@ -1,20 +1,84 @@
 'use strict';
-$(document).ready(function (){
+// $(document).ready(function (){
+
+
+
+
+
+getLocationData(29.42 ,-98);
+
+    function appendWeatherData(data) {
+
+        let weatherBox = $('.weather-box');
+        let currentDayBox = $('.current-day-box');
+
+        currentDayBox.html('');
+        currentDayBox.html(
+            `
+                     <div class="card mt-1">
+                <div class="card-header">
+                    <span>${getTime()}</span>
+                </div>
+                <div class="card-body">
+
+                        <div class="d-flex justify-content-between">
+                            <div>
+                                <div class="bigger-text">${data.current.temp.toFixed(0)}&#176;</div>
+                                <div class="smaller-text">${data.current.weather[0].main}</div>
+                                <div class="smaller-text">Feels like ${data.current.feels_like.toFixed(0)}&#176; Humidity ${data.current.humidity}%</div>
+                            </div>
+                            <div><img src="http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png"  alt="icon"></div>
+                        </div>
+                </div>
+                <div class="card-header">
+                    <button class="btn">btn</button>
+                </div>
+            </div>
+                    `)
+
+
+
+        console.log(data)
+        weatherBox.html('');
+        for (let i = 0; i < data.daily.length; i++) {
+            weatherBox.append(`
+            <div class="weekly-containers d-flex justify-content-around align-items-center border-bottom">
+                <div>
+                    <img src="http://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@2x.png"  alt="icon">
+                </div>
+                <div>
+                    ${getDayOfWeek(getDate(data.daily[i].dt).getDay())}
+                </div>
+                <div><span class="mr-2">Low: ${data.daily[i].temp.min.toFixed(0)}&#176;</span>       High: ${data.daily[i].temp.max.toFixed(0)}&#176;</div>
+            </div>
+            `)
+
+        }
+    }
+
+
 
     function getLocationData (lat, long) {
         $.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely&appid=${OPEN_WEATHER_MAP_KEY}`, {
             units: "imperial"
         }).done(function(data) {
-            console.log(getDate(data.current.dt));
 
-            console.log("Current: " + data.current.temp)
-            console.log("Low: " + data.daily[0].temp.min)
-            console.log("High: " + data.daily[0].temp.max)
-            console.log(data)
-            // console.log(data.current.dt);
-            // console.log('Diving in - here is current information: ', data.current);
-            // console.log('A step further - information for tomorrow: ', data.daily[1]);
-            // console.log(data.timezone);
+            //current days, info
+
+            // console.log(getDate(data.current.dt));
+            // console.log("Current: " + data.current.temp)
+            // console.log("Low: " + data.daily[0].temp.min)
+            // console.log("High: " + data.daily[0].temp.max)
+            // console.log(data)
+            //
+            // //rest of weeks info
+            // for (let i = 0; i < data.daily.length; i++) {
+            //     let daysData = data.daily[i]
+            //     console.log(getDate(daysData.dt));
+            //     console.log("Low: " + daysData.temp.min);
+            //     console.log("High: " + daysData.temp.max);
+            // }
+            appendWeatherData(data)
         });
     }
 
@@ -35,8 +99,7 @@ $(document).ready(function (){
         console.log("high: " + high);
     }
 
-    function getDayOfWeek () {
-        let day = myDate.getDay();
+    function getDayOfWeek (day) {
 
         if (day === 0)
             return 'Sunday';
@@ -63,32 +126,43 @@ $(document).ready(function (){
         return date;
     }
 
+    function getTime() {
+        let now = new Date();
+        let hours = now.getHours();
+        console.log(hours)
+        var time = now.getHours() + ":" + now.getMinutes();
 
 
 
+        if(now.getHours() > 12){
+            hours -= 12
+            return hours + ":" + now.getMinutes();
+        }
+        return hours + ":" + now.getMinutes();
+    }
 
-
-    getLocationData(24, -98);
-    getLocationData(30, 100)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(getTime())
 
 
 
 
 
 
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// });
